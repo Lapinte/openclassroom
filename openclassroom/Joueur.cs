223806a4adcfcf0f9e1a2feffeb281aaf4425b94
Fati <Adminl@EMER06>
2018-06-05 13:36:04 +0200

    classe joueur

---
+++ openclassroom/Joueur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openclassroom
+{
+    class Joueur
+    {
+        private int pointDeVie;
+
+        public int PointDeVie
+        {
+            get
+            {
+                return pointDeVie;
+            }
+        }
+    }
+}
--- openclassroom/Program.cs
+++ openclassroom/Program.cs
@@ -6,7 +6,7 @@ namespace openclassroom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
