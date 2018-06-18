3b2ed70167a927ef7f78b8012cabde4621201970
Fati <Adminl@EMER04>
2018-06-18 09:37:30 +0200

    Création classe boss de fin

---
+++ openclassroom/BossDeFin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openclassroom
+{
+    public class BossDeFin
+    {
+        private De de;
+        public int PointDeVie { get; private set; }
+        public bool EstVivant
+        {
+            get
+            {
+                return (PointDeVie > 0);
+            }
+        }
+
+        private void Attaquer (Joueur joueur)
+        {
+
+        }
+    }
+}
